676a20be7f6b191cc8442a052d7889cc97a980e7
Zayduck <zaymonnekent17@gmail.com>
2026-02-04 21:01:41 +0800

    bg cleaned

---
+++ src/routes/forgot-password/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import cctBg from "$lib/assets/cct-bg.jpg"
+    import SigninForm from "$lib/components/grouped/SigninForm.svelte";
+</script>
+
+<div
+style="background-image: url('{cctBg}')"
+    class="
+    w-full h-full overflow-x-hidden overflow-y-auto p-8 bg-white/75 space-y-8 items-center flex flex-col bg-center bg-no-repeat bg-cover"
+>
+
+</div>
